9fb0d8d10bea66df2403df3f936694b634c44e00
Unknown <lexi.hougaard@gmail.com>
2017-11-15 20:19:59 -0700

    We did a lot with GUNS

---
+++ Chicken Game/Assets/Scripts/wolfHealth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfHealth : MonoBehaviour {
+
+	public int currentHealth;
+	public int maxHealth = 3;
+	public Transform spawnpoint;
+	public int points;
+
+	public void TakeDamage(int amount){
+		currentHealth -= amount;
+		if(currentHealth <= 0){
+			//Keep score at zero
+			currentHealth = 0;
+			print("wolf is dead!");
+			//Add points to score for killing wolf
+			scoreManager.AddPoints(points);
+			//Move wolf to spawn point
+			transform.position = spawnpoint.position;
+			transform.rotation = spawnpoint.rotation;
+			//Reset wolf health
+			currentHealth = maxHealth;
+		}
+
+	}
+
+}
